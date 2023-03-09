ddc71520e27664eabf756f3e3b0d39b055ec5cb9
AndreyKapus <kapustnikov@ukr.net>
2023-03-09 18:27:58 +0200

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports ={
     mode: 'development',
@@ -7,6 +8,20 @@ module.exports ={
         path: path.resolve(__dirname, 'build'),
         filename: 'webpack.bundle.js',
       },
+      module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+              },
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+              }
+        ]
+      },
+      plugins: [new HtmlWebpackPlugin({ template: "src/index.html"})],
     devServer: {
         static: {
             directory: path.join(__dirname, '/')
